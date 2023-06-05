ba0fde7a83268ec45cec82ac46412793b65b8086
reilolmsted <ROlmsted8132@student.whatcom.edu>
2023-06-04 19:30:06 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class Main{
+    public static void main (String [] args){
+        WarGame d = new WarGame();
+        d.menu();
+        //Menu m = new Menu();
+
+
+    }
+}
+
